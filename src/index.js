ac64803dd3276808207c0c8093ff92294e1d600b
yangfan <yangfan44777@163.com>
2018-10-27 20:03:41 +0800

    update resizer

--- src/index.js
+++ src/index.js
@@ -17,25 +17,57 @@ PIXI.loader.add('resources', spritesheet.url).load((loader, resources) => {
     // This creates a texture from a 'bunny.png' image
     const bunny = new PIXI.Sprite(resources['resources'].textures['bunny.png']);
  
-    
+    const container = new PIXI.Container();    
 
     // Rotate around the center
     bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
+    bunny.anchor.y = 0.4;
+
+    container.x = 300;
+    container.y = 600;
+
+    container.addChild(bunny);
+
+
+    const bunny2 = new PIXI.Sprite(resources['resources'].textures['bunny.png']);
+ 
+    bunny2.anchor.x = -0.2;
+    bunny2.anchor.y = -0.5;
+
+    bunny2.scale.x = 2.5;
+    bunny2.scale.y = 5;
+    bunny2.x = 200;
+    bunny2.y = 800;
+
 // 
+
+    bunny.interactive = true;
+    bunny2.interactive = true;
+    bunny.on('pointerdown', () => {
+        resizer.bind(bunny);
+    });
+    bunny2.on('tap', () => {
+        resizer.bind(bunny2);
+    })
 
     bunny.scale.x = 5;
     bunny.scale.y = 5;
+    bunny.x = 300;
+    bunny.y = 500;
+    container.rotation = 0.3;
     const resizer = new Resizer(bunny, {
-        x: app.screen.width / 2,
-        y: app.screen.height / 2
-    });
+        borderSize: 10
+    }, app);
     // resizer.scale.x *= 4.25;
     // resizer.scale.y *= 4.25;
     
     // resizer.hide();
-
+    window.resize = resizer;
+    window.bunny = bunny;
+    window.bunny2 = bunny2;
+    app.stage.addChild(container, bunny2);
     app.stage.addChild(resizer);
+    
     
     
     // // Add the bunny to the scene we are building
--- src/lib/Resizer.js
+++ src/lib/Resizer.js
@@ -4,30 +4,63 @@
 import * as PIXI from 'pixi.js'
 import vec3 from 'gl-vec3';
 
-export default class Resizer extends PIXI.Container {
+export default class Resizer extends PIXI.Sprite {
     
-    constructor(resizeElement, options) {
+    constructor(resizeElement, options, app) {
         super();
-
+        this.app = app;
         this.options = {
-            x: 0,
-            y: 0,
+            borderSize: 4,
             ...options
         };
-        this.x = this.options.x;
-        this.y = this.options.y;
 
         this.interactive = true;
-
-        this.resizeElement = resizeElement; 
         this.rectGraphic = new PIXI.Graphics();
-        this.selfGlobalPositionCache = this.getGlobalPosition();
-        
-        this.initHandlers();
-        this.updateRect();
 
+        this.initHandlers();
         this.initEvents();
-        this.addChild(resizeElement, this.rectGraphic);
+        this.addChild(this.rectGraphic);
+
+        
+        this.bind(resizeElement);
+        this.drawAnchor();
+    }
+
+    bind(resizeElement) {
+        
+        if (!resizeElement) {
+            return;
+        }
+
+        this.resizeElement = resizeElement;
+
+        // 强制更新worldTransform
+        resizeElement.toGlobal(new PIXI.Point(0, 0));
+
+        const worldTransform = this.resizeElement.worldTransform.clone()
+        const transform = new PIXI.Transform();
+        worldTransform.decompose(transform);
+
+        this.x = transform.position.x;
+        this.y = transform.position.y;
+        this.anchor.x = this.resizeElement.anchor.x;
+        this.anchor.y = this.resizeElement.anchor.y;
+        this.rotation = transform.rotation;
+        this.rectWidth = resizeElement.width;
+        this.rectHeight = resizeElement.height;
+        this.relativeRotation = resizeElement.rotation - this.rotation;
+    
+        this.updateRect();
+        this.show();
+        
+    }
+
+    unbind() {
+        if (this.resizeElement) {
+            this.resizeElement = null;
+        }
+        this.updateRect();
+        this.hide();
     }
 
     initHandlers() {
@@ -43,28 +76,58 @@ export default class Resizer extends PIXI.Container {
     }
 
     hide() {
-        this.resizeHandler.visible = this.rotateHandler.visible = this.rectGraphic.visible = false;
+        this.visible = false;
     }
 
     show() {
-        this.resizeHandler.visible = this.rotateHandler.visible = this.rectGraphic.visible = true;
+        this.visible = true;
+    }
+
+    drawAnchor() {
+
+        const length = 15;
+        const crossGraphic = new PIXI.Graphics();
+        
+        crossGraphic.lineStyle(5, 0xFFFFFF, 1);
+
+        crossGraphic.moveTo(-length, 0);
+        crossGraphic.lineTo(length, 0);
+
+        crossGraphic.moveTo(0, -length);
+        crossGraphic.lineTo(0, length);
+
+        this.addChild(crossGraphic);
     }
 
     updateRect() {
+
+        const {
+            borderSize,
+        } = this.options;
+        let width;
+        let height;
+
+        if (this.resizeElement) {
+            width = this.resizeElement.width;
+            height = this.resizeElement.height;
+        } else {
+            width = 0;
+            height = 0;
+        }
         this.rectGraphic.clear();
         this.rectGraphic.beginFill(0xFFFFFF, 0);
-        this.rectGraphic.lineStyle(4, 0xFFFFFF, 1);
-        this.rectGraphic.drawRect(0, 0, this.resizeElement.width, this.resizeElement.height);
-        this.rectGraphic.pivot.x = this.rectGraphic.width / 2;
-        this.rectGraphic.pivot.y = this.rectGraphic.height / 2;
+        this.rectGraphic.lineStyle(borderSize, 0xFFFFFF, 1);
+
         
+        this.rectGraphic.drawRect(-width * this.anchor.x, -height * this.anchor.y, width, height);
+
         // 重置resizeHandler位置
-        this.resizeHandler.x = -this.rectGraphic.width / 2;
-        this.resizeHandler.y = -this.rectGraphic.height / 2;
+        this.resizeHandler.x = -width * this.anchor.x;
+        this.resizeHandler.y = -height * this.anchor.y;
         
         // 重置rotateHandler位置
-        this.rotateHandler.x = 0;
-        this.rotateHandler.y = this.rectGraphic.height / 2 + 50;
+        this.rotateHandler.x = width * (1 - this.anchor.x);
+        this.rotateHandler.y = height * (1 - this.anchor.y);
     }
 
     dragable(item, options) {
@@ -79,6 +142,10 @@ export default class Resizer extends PIXI.Container {
             this.data = event.data;
             this.dragging = true;
             this.startPosition = this.data.getLocalPosition(this.parent);
+            this.relativePosition = {
+                x: this.startPosition.x - this.x,
+                y: this.startPosition.y - this.y
+            }
             options && options.onDragStart && options.onDragStart(this.startPosition);
         }
 
@@ -91,8 +158,8 @@ export default class Resizer extends PIXI.Container {
         function onDragMove(event) {
             if (this.dragging) {
                 var newPosition = this.data.getLocalPosition(this.parent);
-                this.x = newPosition.x;
-                this.y = newPosition.y;
+                this.x = newPosition.x - this.relativePosition.x;
+                this.y = newPosition.y - this.relativePosition.y;
                 options && options.onDragMove && options.onDragMove(newPosition, this.startPosition, event.data.global);
             }
         }
@@ -102,30 +169,70 @@ export default class Resizer extends PIXI.Container {
 
         this.dragable(this.resizeHandler, {
             onDragStart: () => {
-                this.resizeElement.startSize = {
+                if (!this.resizeElement) {
+                    return;
+                }
+                this.startSize = {
                     width: this.resizeElement.width,
                     height: this.resizeElement.height,
                 };
             },
             onDragMove: (newPosition, startPosition) => {
-                const newWidth = this.resizeElement.startSize.width + (-newPosition.x + startPosition.x) * 2;
-                const newHeight = this.resizeElement.startSize.height + (-newPosition.y + startPosition.y) * 2;
+                if (!this.resizeElement) {
+                    return;
+                }
+
+                let newWidth;
+                let newHeight;
+
+                if (this.anchor.x === 0) {
+                    newWidth = this.startSize.width;
+                } else {
+                    newWidth = this.startSize.width + (-newPosition.x + startPosition.x) / this.anchor.x;
+                }
+                if (this.anchor.y === 0) {
+                    newHeight = this.startSize.height;
+                } else {
+                    newHeight = this.startSize.height + (-newPosition.y + startPosition.y) / this.anchor.y;
+                }
                 this.resizeElement.width = newWidth <= 0 ? 1 : newWidth;
                 this.resizeElement.height = newHeight <= 0 ? 1 : newHeight;
+
                 this.updateRect();
             }
         });
         
         this.dragable(this.rotateHandler, {
-            onDragStart: () => {
+            onDragStart: (startPosition) => {
                 this.selfGlobalPositionCache = this.getGlobalPosition();
+                
+                if (this.anchor.x === 0) {
+                    this.a = this.resizeElement.height / this.resizeElement.width;
+                } else {
+                    this.a = (this.resizeElement.height * (1 - this.anchor.y)) / (this.resizeElement.width * (1 - this.anchor.x));
+                }
+            
+                console.log('a:', this.a);
             },
             onDragMove: (newPosition, startPosition, globalPosition) => {
-                this.rotation = (globalPosition.x - this.selfGlobalPositionCache.x > 0 ? -1 : 1) * vec3.angle([globalPosition.x - this.selfGlobalPositionCache.x, globalPosition.y - this.selfGlobalPositionCache.y, 1], [0, 100, 1]);
+                
+                const y = this.a * (globalPosition.x - this.selfGlobalPositionCache.x);
+                const direct = (globalPosition.y - this.selfGlobalPositionCache.y - y) > 0 ? 1 : -1;
+                this.rotation = direct * vec3.angle([globalPosition.x - this.selfGlobalPositionCache.x, globalPosition.y - this.selfGlobalPositionCache.y, 1], [this.resizeElement.width * (1 - this.anchor.x), this.resizeElement.height * (1 - this.anchor.y), 1]);
+
+                this.resizeElement.rotation = this.relativeRotation + this.rotation;
+
                 this.updateRect();
             }
         });
-        this.dragable(this);
+        this.dragable(this, {
+            onDragMove: () => {
+                const localPoint = this.resizeElement.parent.toLocal(new PIXI.Point(this.parent.x, this.parent.y), this);
+
+                this.resizeElement.x = localPoint.x;
+                this.resizeElement.y = localPoint.y;
+            }
+        });
     }
 
     
